3fd4ba606b81c47519da4c04fbf181aad40f2642
Pandragona <asiuspospolitus@gmail.com>
2019-02-10 15:55:08 +0100

    RoootController

---
+++ src/main/java/pl/sda/openweather/controller/RootController.java
@@ -0,0 +1,56 @@
+package pl.sda.openweather.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import pl.sda.openweather.model.Weather;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RootController implements Initializable {
+
+    private String finalURL = "http://api.apixu.com/v1/current.json?key=a147a586068c46f2b4e90949191002&q=";
+
+    @FXML
+    private Button search;
+
+    @FXML
+    private TextField city;
+
+    @FXML
+    private Label tempResult;
+
+    @FXML
+    private Label feelTempResult;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        city.setText("nazwa miasta");
+    }
+
+    public void setCity(ActionEvent actionEvent) {
+        try {
+            URL jsonURL = new URL(finalURL + city.getText());
+
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            Weather weather = objectMapper.readValue(jsonURL, Weather.class);
+            feelTempResult.setText(String.valueOf(weather.getCurrent().getFeelslike_c()));
+            tempResult.setText(String.valueOf(weather.getCurrent().getTemp_c()));
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
